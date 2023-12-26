862069bb08b06448670ebe85db60352adf3f68d8
Yugaprasanna <yuga27112000@gmail.com>
2023-12-26 16:44:23 +0530

    fff

--- src/pages/home/New.js
+++ src/pages/home/New.js
@@ -13,7 +13,7 @@ const ComprehensiveWellness = ({ spaceTopClass, spaceBottomClass, dateTime }) =>
           <div className="col-md-6 col-xs-6 col-lg-6 order-1 order-lg-1">
             <div className="funfact-content funfact-res text-left">
             <h3 className="f-60">Embrace Comprehensive Wellness</h3>
-              <p className="pt-4 home">Rezingo encompasses all the dimensions of wellness, guiding your journey towards a healthier, more balanced life.</p>
+              <p className="pt-4 home">The Rezingo encompasses all the dimensions of wellness, guiding your journey towards a healthier, more balanced life.</p>
               {/* ll<div id="cta-waitlist-input" class="mb-12" style={{position:" relative"}}>
                 <input type="email" class="mb-2" style={{    paddingTop: "1.5rem",height:"65px", paddingBottom: "1.5rem",paddingRight: "5rem",paddingLeft: "1.5rem"}} placeholder="Enter your email address"/>
    */}
